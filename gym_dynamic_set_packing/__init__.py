c84cc06b45dddcfbe28082e9835553af2ffff5ad
Michael J. Curry <curry@cs.umd.edu>
2019-02-04 13:57:57 -0500

    start of abstract class for 0-1 matching

--- gym_dynamic_set_packing/__init__.py
+++ gym_dynamic_set_packing/__init__.py
@@ -3,4 +3,4 @@ from gym.envs.registration import register
 
 logger = logging.getLogger(__name__)
 
-register(id='DynamicSetPacking-v0', entry_point='gym_dynamic_set_packing.envs:DynamicSetPackingEnv')
+register(id='DynamicSetPacking-binary-v0', entry_point='gym_dynamic_set_packing.envs:DynamicSetPackingBinaryEnv')
--- gym_dynamic_set_packing/envs/dynamic_set_packing_env.py
+++ gym_dynamic_set_packing/envs/dynamic_set_packing_env.py
@@ -3,19 +3,62 @@ from gym import spaces
 import numpy as np
 import random
 
-class DynamicSetPackingEnv(gym.Env):
+class DynamicSetPackingBinaryEnv(gym.Env):
 
-    def __init__(self):
-        raise NotImplementedError
+    """
+    Abstract class representing a dynamic set packing problem with 0/1 matching.
+    """
+    def __init__(self, state_dim):
+        self.state_dim = state_dim
+        self.action_space = spaces.Discrete(1)
+        self.observation_space = spaces.Box(
+                low=np.zeros(state_dim),
+                high=np.full(state_dim, np.inf))
+        self.seed()
+
+        self.state = None
+        self.reset()
+
 
     def step(self, action):
+        assert self.action_space.contains(action)
+
+        reward = 0.0
+        if action == 1:
+            chosen_match = self._perform_match(self.state)
+            reward = self._run_match(chosen_match)
+
+
+        # elements arrive and depart
+        self._arrive_and_depart()
+
+        # do we need these dummy values?
+        info = {'no info': 'no info'}
+        done = False
+
+        return self.state, reward, done, info
+
+    ## the following MUST be implemented in the child
+    def _run_match(self, match):
+        """Must be overridden by a child. Takes the output of the match solver and uses it to update the state, possibly with some probability of failure. Returns the reward from the match."""
+        raise NotImplementedError
+
+    def _perform_match(self, state):
+        "Takes a state and finds a match to return. Does not actually modify state."
         raise NotImplementedError
 
     def reset(self):
+        """Initializes the environment. Must be provided by child."""
         raise NotImplementedError
 
-    def _render(self, mode='human', close=False):
-        raise NotImplementedError
+    ## the following MAY be overridden by a child
+    def _arrive_and_depart(self):
+        "Simualtes arrival and departure of elements. May be overridden by child."
+        pass
+
+    def render(self, mode='human', close=False):
+        "Just prints out the current state vector. May be overriden by child if desired."
+        return self.state
 
     def seed(self, seed):
         random.seed(seed)

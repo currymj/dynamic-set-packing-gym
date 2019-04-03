e26c5b2aa28ef9615000f6b700121fed09006fc0
Michael J. Curry <curry@cs.umd.edu>
2019-04-03 13:56:02 -0400

    start of code for weighted matcher

--- gym_dynamic_set_packing/matchers/matcher.py
+++ gym_dynamic_set_packing/matchers/matcher.py
@@ -6,7 +6,7 @@ import numpy as np
 # the user of the matcher should sum these up, since they
 # may want to do some randomization to simulate match failures
 
-class Matcher:
+class BinaryMatcher:
     def __init__(self, valid_sets):
         #Valid sets should be a (dense?) numpy matrix.
         # we think in terms of column vectors
@@ -15,7 +15,16 @@ class Matcher:
     def match(self, state):
         raise NotImplementedError
 
-class GurobiMatcher(Matcher):
+class WeightedMatcher:
+    def __init__(self, valid_sets):
+        #Valid sets should be a (dense?) numpy matrix.
+        # we think in terms of column vectors
+        self.valid_sets = valid_sets
+
+    def match(self, state, weights):
+        raise NotImplementedError
+
+class GurobiMatcher(BinaryMatcher):
     def __init__(self, valid_sets, show_output=False):
         super(GurobiMatcher, self).__init__(valid_sets)
         self.show_output = show_output
@@ -28,6 +37,7 @@ class GurobiMatcher(Matcher):
         self.m.setObjective(gr.quicksum(self.row_sums), gr.GRB.MAXIMIZE)
         for i, row_sum in enumerate(self.row_sums):
             self.m.addConstr(row_sum <= 0, name='rowsum_{}'.format(i))
+            
         self.m.update()
 
     def match(self, state):
@@ -40,10 +50,30 @@ class GurobiMatcher(Matcher):
         solns = [x.x for x in self.x_vars]
         return solns
 
-class WeightedMatcher:
+class GurobiWeightedMatcher(WeightedMatcher):
+    def __init__(self, valid_sets, show_output=False):
+        super(GurobiMatcher, self).__init__(valid_sets)
+        self.show_output = show_output
+        self.n_types = self.valid_sets.shape[0]
+        self.n_sets = self.valid_sets.shape[1]
+        self.m = gr.Model("match")
+        self.m.setParam('OutputFlag', self.show_output)
+        self.x_vars = [self.m.addVar(vtype=gr.GRB.INTEGER, lb=0, name='x_{}'.format(i)) for i in range(self.n_sets)]
+        self.row_sums = [gr.LinExpr(self.valid_sets[i,:], self.x_vars) for i in range(self.n_types)]
+        self.type_weights = np.ones(self.n_types)
+        self.m.setObjective(gr.quicksum(self.row_sums), gr.GRB.MAXIMIZE)
+        for i, row_sum in enumerate(self.row_sums):
+            self.m.addConstr(row_sum <= 0, name='rowsum_{}'.format(i))
+        self.m.update()
 
-    def __init__(self):
-        raise NotImplementedError
+    def match(self, state, action):
+        "Assume state is a 1d numpy vector of counts per type"
+        for i in range(len(self.row_sums)):
+            constr = self.m.getConstrByName('rowsum_{}'.format(i))
+            constr.setAttr(gr.GRB.Attr.RHS, state[i])
+        
+        self.m.setObjective(gr.quicksum(row_sum[i] * action[i] for i in range(len(self.n_types))), gr.GRB.MAXIMIZE)
+        self.m.optimize()
+        solns = [x.x for x in self.x_vars]
+        return solns
 
-    def match(self, state, type_weights):
-        raise NotImplementedError
